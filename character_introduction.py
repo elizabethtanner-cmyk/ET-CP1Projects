7723fcfe58d470964bc14ed8c5ce9cc7787d4b4e
elizabethtanner-cmyk <elizabeth.tanner@ucas-edu.net>
2026-08-20 09:30:55 -0600

    created a character intro using variables

---
+++ character_introduction.py
@@ -0,0 +1,7 @@
+#Elizabeth Tanner Character introduction
+
+name = 'Bob'
+age = '37'
+job = 'software engineer'
+location_from = 'Utah'
+print(f'{name} is a {age}-year-old {job} who is from {location_from}.')
