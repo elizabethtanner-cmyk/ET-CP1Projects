30dc690d452ae3804eeac10753f426ba4956a596
elizabethtanner-cmyk <elizabeth.tanner@ucas-edu.net>
2026-08-24 09:09:22 -0600

    debugged the given code and saved it as debugging.py

---
+++ debugging.py
@@ -0,0 +1,5 @@
+name = "Alex"
+age = 16
+#name is spelled wrong (it was spelled as nam)
+#print statement didn't have a close parenthese)
+print("My name is " + name + " and I am " + str(age) + " years old.")
