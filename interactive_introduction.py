cae0ade907bac5cdf041cf529b86a7de37fabd33
elizabethtanner-cmyk <elizabeth.tanner@ucas-edu.net>
2026-08-26 09:19:05 -0600

    created the interactive introduction

---
+++ interactive_introduction.py
@@ -0,0 +1,8 @@
+#Elizabeth Tanner Interactive Introduction
+
+full_name = input('What is your full name?\n')
+favorite_color = input('What is your favorite color?\n')
+age = input('How old are you?\n')
+dream_job = input('What do you want to be more than anything else (in work)?\n')
+
+print(f'{full_name}, who is {age} years old, loves the color {favorite_color} and wants to be a {dream_job}.')
